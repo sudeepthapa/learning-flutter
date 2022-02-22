d9732f60dfa759d904e887588af96ac1c79c66b4
Sudip Thapa <sudeptech@gmail.com>
2022-02-22 09:29:48 +0545

    form element handled

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class FormScreen extends StatelessWidget {
+class FormScreen extends StatefulWidget {
   const FormScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  final TextEditingController _usernameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +16,61 @@ class FormScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('FormScreen'),
       ),
-      body: const Center(child: Text('FormScreen')),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          TextField(
+            controller: _usernameController,
+            decoration: InputDecoration(
+              labelText: 'Username',
+              icon: Icon(Icons.person),
+              helperText: 'Email and username accepted.',
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          TextField(
+            textCapitalization: TextCapitalization.sentences,
+            maxLines: 3,
+            decoration: InputDecoration(
+              labelText: 'Bio',
+              icon: Icon(Icons.biotech),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          TextField(
+            maxLines: 1,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              icon: Icon(Icons.lock),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          TextField(
+            maxLines: 1,
+            keyboardType: TextInputType.phone,
+            decoration: InputDecoration(
+              labelText: 'Phone',
+              icon: Icon(Icons.phone),
+            ),
+          ),
+          SizedBox(
+            height: 35,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              print(_usernameController.text);
+            },
+            child: Text('Submit'),
+          )
+        ],
+      ),
     );
   }
 }
