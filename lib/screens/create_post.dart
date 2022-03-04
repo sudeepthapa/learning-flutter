cc0956915ba24d69638886c551e13d5c50fe0deb
Sudip Thapa <sudeptech@gmail.com>
2022-03-04 09:03:39 +0545

    form handling

--- lib/screens/create_post.dart
+++ lib/screens/create_post.dart
@@ -1,12 +1,76 @@
 import 'package:flutter/material.dart';
 
-class CreatePost extends StatelessWidget {
+class CreatePost extends StatefulWidget {
   const CreatePost({Key? key}) : super(key: key);
 
   @override
+  State<CreatePost> createState() => _CreatePostState();
+}
+
+class _CreatePostState extends State<CreatePost> {
+  // Global Key
+  final _formKey = GlobalKey<FormState>();
+
+  // Controllers
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    titleController.dispose();
+    descriptionController.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // titleController.text = 'abx';
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Create Post Screen'),
+    return Form(
+      key: _formKey,
+      child: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        children: [
+          TextFormField(
+            controller: titleController,
+            decoration: const InputDecoration(
+              icon: Icon(Icons.title_outlined),
+              hintText: 'Post title',
+              labelText: 'Post title',
+            ),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter title.';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            maxLines: 3,
+            controller: descriptionController,
+            decoration: const InputDecoration(
+              icon: Icon(Icons.description),
+              hintText: 'Description',
+              labelText: 'Description',
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+              } else {}
+            },
+            child: const Text('Post'),
+          )
+        ],
+      ),
     );
   }
 }
