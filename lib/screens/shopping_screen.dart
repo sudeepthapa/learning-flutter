95b90b18e1efbb59bcc33a52ef59810e841d8935
Sudip Thapa <sudeptech@gmail.com>
2022-03-04 08:37:50 +0545

    tab bar view implemented

--- lib/screens/shopping_screen.dart
+++ lib/screens/shopping_screen.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:myfirstapp/screens/create_post.dart';
+import 'package:myfirstapp/screens/search_screen.dart';
 
 class ShoppingScreen extends StatelessWidget {
   const ShoppingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Shopping Screen'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(
+                  Icons.baby_changing_station,
+                ),
+                text: 'Baby',
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.railway_alert,
+                ),
+                text: 'Railway',
+              )
+            ],
+          ),
+        ),
+        body: const TabBarView(children: [SearchScreen(), CreatePost()]),
+      ),
     );
   }
 }
