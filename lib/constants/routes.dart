e1f5b0ef4cd416a1b868031ea17d3fe248dd5dc6
Sudip Thapa <sudeptech@gmail.com>
2022-03-14 20:20:49 +0545

    dynamic routing handled

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -13,10 +13,29 @@ class Routes {
   static const String ROOT = '/';
   static const String POSTS = '/posts';
 
+  // Named Routes
   static Map<String, WidgetBuilder> getRoutes() {
     return {
       Routes.ROOT: (_) => const Home(),
       Routes.POSTS: (_) => const PostScreen()
     };
+  }
+
+  // Dynamic routing
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.ROOT:
+        return MaterialPageRoute(builder: (_) => const Home());
+      case Routes.POSTS:
+        return MaterialPageRoute(builder: (_) => const PostScreen());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => const Scaffold(
+            body: Center(
+              child: Text('No route defined'),
+            ),
+          ),
+        );
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.amber,
       ),
       initialRoute: Routes.ROOT,
-      routes: Routes.getRoutes(),
+      onGenerateRoute: Routes.generateRoute,
     );
   }
 }
