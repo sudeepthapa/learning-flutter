03f62878e94570aae5a1459df5bfbb3680c961ab
Sudip Thapa <sudeptech@gmail.com>
2022-03-14 20:57:30 +0545

    todo app done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,26 +1,60 @@
-import 'dart:io';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:myfirstapp/screens/posts_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _titleController = TextEditingController();
+  List<String> todos = [];
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Platform.isIOS
-          ? CupertinoButton(
-              child: const Text('Click Me'),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const PostScreen()));
-              },
-            )
-          : ElevatedButton(onPressed: () {}, child: const Text('Click here')),
+    return Container(
+      padding: const EdgeInsets.all(15),
+      width: double.maxFinite,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Flexible(
+                child: TextField(
+                  controller: _titleController,
+                  decoration: const InputDecoration(
+                    labelText: 'Enter something',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      todos = [...todos, _titleController.text];
+                    });
+                    _titleController.clear();
+                  },
+                  icon: const Icon(Icons.check))
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Flexible(
+            child: todos.isEmpty
+                ? const Center(
+                    child: Text('No todos at the moment'),
+                  )
+                : ListView.builder(
+                    itemCount: todos.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Text(todos[index]);
+                    },
+                  ),
+          )
+        ],
+      ),
     );
   }
 }
