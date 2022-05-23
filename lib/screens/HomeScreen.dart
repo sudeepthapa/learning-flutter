4d1b8a65ab015afd14f8ec895a390df385f0a815
Sudip Thapa <sudeptech@gmail.com>
2022-05-23 21:06:36 +0545

    welcome text added

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -36,7 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(username),
+        title: Text('Welcome $username'),
         actions: [
           IconButton(
             onPressed: () async {
